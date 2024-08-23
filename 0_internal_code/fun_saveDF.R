faa28c7fca40f9120fb2ff4ca969fa55789879b5
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-23 16:14:53 +0300

    Update fun_saveDF.R

--- 0_internal_code/fun_saveDF.R
+++ 0_internal_code/fun_saveDF.R
@@ -1,17 +1,19 @@
-fun_saveDF <- function(z, kop, DF, j) {
+fun_saveDF <- function(z, kop, df, j) {
   
-  DF_name <- paste0(kop, "_t", DF[j])
+  DF_name <- paste0(kop, "_t", df)
   assign(DF_name, z)
   
-  p <- paste0(path, "1_starptabulas\\tabulas\\")
-  if(!dir.exists(p)) {dir.create(p)}
-  save(list = DF_name, file = paste0(p, tab_name, "_", DF_name, ".RData"))
+  #p <- paste0(path, "1_starptabulas\\tabulas\\")
+  #if(!dir.exists(p)) {dir.create(p)}
+  #save(list = DF_name, file = paste0(p, tab_name, "_", DF_name, ".RData"))
   
-  p <- paste0(path, "2_gala_tabulas\\")
-  if(!dir.exists(p)) {dir.create(p)}
-  write.table(z, file = paste0(p, tab_name, "_", DF_name, ".csv"), sep = ";", 
-            col.names = FALSE, qmethod = "double", row.names = FALSE)
+  # Pāriet uz izstrādes mapi un saglabā tabulu.
+  path <- paste0(base_path, year, "Q", Q, "\\2_izstrade\\")
+  if(!dir.exists(path)) {dir.create(path)}
+  
+  write.table(z, file = paste0(path, tab_name, "_", DF_name, ".csv"), sep = ";", 
+            col.names = TRUE, qmethod = "double", row.names = FALSE)
   
   rm(list = DF_name, DF_name, z, kop)
-  return(paste0("Gala tabula Nr.", DF[j], " saglabāta:\n", p, "\n"))
+  return(paste0("Tabula Nr.", DF[j], " gatava.\n"))
 }
