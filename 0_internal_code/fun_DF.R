fdfe2aca873f0b80dc1c158a8a2c117a34a3eb27
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-08-23 13:54:54 +0300

    Update fun_DF.R

--- 0_internal_code/fun_DF.R
+++ 0_internal_code/fun_DF.R
@@ -7,25 +7,22 @@ fun_DF <- function(x, k_fm, order_vec, kop) {
     y <- x[x$NTABL == DF[j],]
     
     #2 Atlasa dubultniekus no NACE
-    y <- y[y$NOZARE2 == unique(y$NOZARE2),]
+    y <- y[y$NOZ2 == unique(y$NOZ2),]
     
     #3 Savieno tabulu ar konfidencialitātes tabulu
-    mergeDF <- merge(y, k_fm, by.x = "NOZARE2", by.y = "NOZARE2")
+    mergeDF <- merge(y, k_fm, by.x = "NOZ2", by.y = "NOZ2")
     
     #4 Uzreiz sakārto NACE kodus
-    mergeDF <- mergeDF[match(order_vec, mergeDF$NOZARE2),]
+    mergeDF <- mergeDF[match(order_vec, mergeDF$NOZ2),]
     rownames(mergeDF) <- NULL
     z <- mergeDF
     
     #5 Pārsauc ailes un izformē
-    z <- fun_formatDF(z, DF[j])
+    z <- fun_formatDF(z, DF[j], order_vec)
     
     #6 Saglabā
     cat(fun_saveDF(z, kop, DF, j))
   }
   rm(x, y, DF, k_fm, order_vec, j, m, q, mergeDF)
   return(paste0("Tabulas kopsavilkumam ", substr(kop, 3, 4), " gatavas.\n"))
-}
-  rm(x, y, DF, k_fm, order_vec, j, m, q, mergeDF)
-  return(paste0("Tabulas kopsi ", substr(kop, 3, 4), " gatavas.\n"))
 }
