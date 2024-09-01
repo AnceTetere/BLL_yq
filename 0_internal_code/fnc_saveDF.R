d262df0449a6b3bcea737cb945b4bc43869eb541
Ance Tetere <114180753+AnceTetere@users.noreply.github.com>
2024-09-01 22:27:50 +0300

    Create fnc_saveDF.R

---
+++ 0_internal_code/fnc_saveDF.R
@@ -0,0 +1,15 @@
+fnc_saveDF <- function(z, kop, df, j) {
+  
+  DF_name <- paste0(kop, "_t", df)
+  assign(DF_name, z)
+  
+  # Pāriet uz izstrādes mapi un saglabā nT.
+  path <- paste0(base_path, year, "Q", Q, "\\2_izstrade\\")
+  if(!dir.exists(path)) {dir.create(path)}
+  
+  write.table(z, file = paste0(path, tab_name, "_", DF_name, ".csv"), sep = ";", 
+            col.names = TRUE, qmethod = "double", row.names = FALSE)
+  
+  rm(list = DF_name, DF_name, z, kop)
+  return(paste0("nT:", df, " gatava.\n"))
+}
